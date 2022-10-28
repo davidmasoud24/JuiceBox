b35640891ecdcfd76f82b2dba538cb72ad1ea119
Aundrina Leyva <aundrinaleyva@Aundrinas-MacBook-Air.local>
2022-10-28 12:51:54 -0700

    editing posts

--- api/posts.js
+++ api/posts.js
@@ -1,11 +1,31 @@
 const express = require("express");
 const postsRouter = express.Router();
 const { getAllPosts } = require("../db");
+const { requireUser } = require("./utils");
+
+postsRouter.post("/", requireUser, async (req, res, next) => {
+  const { title, content, tags = "" } = req.body;
+  const tagArr = tags.trim().split(/\s+/);
+  const postData = {};
+  if (tagArr.length) {
+    postData.tags = tagArr;
+  }
+  try {
+    const postData = { authorId: req.user.id, title, content };
+    const post = await createPost(postData);
+    if (post) {
+      res.send(post);
+    } else {
+      next({ name: "error", message: "this is an error" });
+    }
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
+});
 
 postsRouter.get("/", async (req, res) => {
   try {
     const posts = await getAllPosts();
-
     res.send({
       posts,
     });
